5ed25fb39ce7b332afbbaef50203f17a68f83092
FhuAnn <phuandongthap02@gmail.com>
2026-08-08 21:52:51 +0700

    add config OAuth

---
+++ workforce-management/src/main/java/com/example/workforce_management/config/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.workforce_management.config.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) {
+        return http.authorizeHttpRequests(auth-> {
+            auth.requestMatchers("/").permitAll();
+            auth.anyRequest().authenticated();
+        })
+        .oauth2Login(withDefaults())
+        .formLogin(withDefaults()).
+        build();
+    }
+}
